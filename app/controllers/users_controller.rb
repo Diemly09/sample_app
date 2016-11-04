8efbac65bce913446a5b1722b025cbef72e84c64
Diem Ly <diemly0902@gmail.com>
2016-11-04 17:37:14 +0700

    chap8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find_by params[:id]
+    @user = User.find_by id: params[:id]
     if @user.nil?
       flash[:success] = "Not found ID" 
       redirect_to root_path
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params    
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -24,7 +25,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require :user.permit :name, :email, :password,
+    params.require(:user).permit :name, :email, :password,
       :password_confirmation
   end
 end
