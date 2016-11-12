7d44f8c61f5479d2e5906ac22ac664d1d06e01fe
Diem Ly <diemly0902@gmail.com>
2016-11-12 12:18:17 +0700

    chap 10

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109033206) do
+ActiveRecord::Schema.define(version: 20161111100138) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin", default: false
   end
-
 end
